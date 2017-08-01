da3750fbca3069fbc376f7b0598bd098df8e9046
Gino Saavedra <gino.saavedra@nokia.com>
2017-08-01 10:18:31 +0800

    add profile and search section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { MaterialModule } from '@angular/material'
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/config.ts
@@ -0,0 +1,25 @@
+export class Config {
+    RESULTS = [
+        {
+            firstName: 'Jose',
+            lastName: 'Cruz',
+            City: 'Antipolo',
+            Country: 'Philippines',
+            img: 'http://lorempixel.com/400/200/people/'
+        },
+        {
+            firstName: 'Jose',
+            lastName: 'Cruz',
+            City: 'Antipolo',
+            Country: 'Philippines',
+            img: 'http://lorempixel.com/400/200/people/'
+        },
+        {
+            firstName: 'Jose',
+            lastName: 'Cruz',
+            City: 'Antipolo',
+            Country: 'Philippines',
+            img: 'http://lorempixel.com/400/200/people/'
+        }
+    ];
+}
